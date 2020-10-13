16d7b1f2f7eb31844068b1e7b6ea714f483d78f7
Mitchell Cook <m.cook2434@gmail.com>
2020-10-13 11:24:38 -0500

    added more validation to player table

--- app/models/player.rb
+++ app/models/player.rb
@@ -2,5 +2,6 @@ class Player < ApplicationRecord
   belongs_to :college
   belongs_to :team
 
-  validates :name, presence: true
+  validates :full_name, :games_played, :minutes, :points, :rebounds,
+            :assists, :steals, :blocks, :turnovers, :age, :birth_place, :position, presence: true
 end
