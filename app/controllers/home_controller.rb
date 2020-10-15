a9366e42a5485a8f94ffa099a872ba15c3717f69
Mitchell Cook <m.cook2434@gmail.com>
2020-10-15 17:30:23 -0500

    added a list of players and a list of teams to the home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,9 @@
 class HomeController < ApplicationController
   def index
+    @players = Player.includes(:team)
+                     .order("points DESC")
+                     .limit(10)
+
+    @teams = Team.limit(10)
   end
 end
