39ef3798d1d674990930bbe50666cd3b3b05fe32
Mitchell Cook <m.cook2434@gmail.com>
2020-10-15 15:40:11 -0500

    generated a Players and Teams controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'teams/index'
+  get 'teams/show'
+  get 'players/index'
+  get 'players/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
