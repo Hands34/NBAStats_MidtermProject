a785a4773a69130dec1b1f541304868e7327819d
Mitchell Cook <m.cook2434@gmail.com>
2020-10-15 16:25:48 -0500

    changed routes, added bulma to project, changed index and show page for Player and Team to show name with link to the indiviudal page

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,7 +1,9 @@
 class PlayersController < ApplicationController
   def index
+    @players = Player.includes(:team).order("points DESC")
   end
 
   def show
+    @player = Player.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  get 'teams/index'
-  get 'teams/show'
-  get 'players/index'
-  get 'players/show'
+  resources :players, only: %i[index show]
+  resources :teams, only: %i[index show]
+
+  # get 'teams/index'
+  # get 'teams/show'
+  # get 'players/index'
+  # get 'players/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
