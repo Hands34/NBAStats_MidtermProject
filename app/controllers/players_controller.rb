77ec3cabb71473c4a416f83b02e1c8e845c4c4e8
Mitchell Cook <m.cook2434@gmail.com>
2020-10-24 11:02:28 -0500

    added search bar and functionality, added kaminari gem

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -6,4 +6,13 @@ class PlayersController < ApplicationController
   def show
     @player = Player.find(params[:id])
   end
+  # The @movie variable will be shared with :
+  # app/views/movies/show.html.erb
+
+  def search
+    wildcard_search = "%#{params[:keywords]}%"
+    @players = Player.where("full_name LIKE ?", wildcard_search)
+  end
+  # @movies variable will be shared with:
+  # app/views/moves/search.html.erb
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  resources :players, only: %i[index show]
+  resources :players, only: %i[index show] do
+    collection do
+      get "search"
+    end
+  end
+
   resources :teams, only: %i[index show]
 
   root to: "home#index"
